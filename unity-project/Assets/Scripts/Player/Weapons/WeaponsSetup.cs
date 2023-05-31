51ac9926cf82010cde26f27000cd62145bec8f70
Fictional <fictionalaroma@gmail.com>
2023-05-31 13:48:24 +0100

    remove unused Weapon Model

--- unity-project/Assets/Scripts/Player/Weapons/WeaponsSetup.cs
+++ unity-project/Assets/Scripts/Player/Weapons/WeaponsSetup.cs
@@ -5,7 +5,6 @@ namespace Player.Weapons
 	[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/WeaponSetup", order = 2)]
 	public class WeaponsSetup : ScriptableObject
 	{
-		public GameObject weaponModel;
 		public WeaponMode primary;
         public WeaponMode secondary;
 
